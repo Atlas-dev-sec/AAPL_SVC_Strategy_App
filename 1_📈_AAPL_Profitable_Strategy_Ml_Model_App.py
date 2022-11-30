7829e62b21ab81d5f7770beb3818e3023d272b83
Atlas <113722885+Atlas-dev-sec@users.noreply.github.com>
2022-11-30 14:19:35 -0500

    Update 1_📈_AAPL_Profitable_Strategy_Ml_Model_App.py

--- 1_📈_AAPL_Profitable_Strategy_Ml_Model_App.py
+++ 1_📈_AAPL_Profitable_Strategy_Ml_Model_App.py
@@ -92,7 +92,7 @@ X_input = target[['open-close', 'high-low', 'Close']]
 
 target['pred'] = model.predict(X_input[['open-close', 'high-low']])
 
-#st.write('# Today\'s model prediction is: {}'.format(target.iloc[0, 8]))
+st.write('# Today\'s model prediction is: {}'.format(target))
 
 # MODEL INSTRUCTIONS
 st.title('Strategy Instructions')
